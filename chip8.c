17c37eeb2f6eca81a684e4116fbf1ae05aef6e75
ryawa <22625346+ryawa@users.noreply.github.com>
2026-08-22 17:51:18 -0400

    Implement fetch/decode/execute loop and basic instructions

--- chip8.c
+++ chip8.c
@@ -1,11 +1,110 @@
+#include <stddef.h>
+#include <stdint.h>
+
 #define JS_IMPORT(x) __attribute__((import_module("js"), import_name(x)))
+JS_IMPORT("log") void js_log(const char *str);
+JS_IMPORT("logInstruction") void js_log_instruction(uint16_t inst);
+JS_IMPORT("error") void js_error(const char *str);
+JS_IMPORT("refreshDisplay") void js_refresh_display();
 
-JS_IMPORT("log") void js_log(const char* str);
+#define WIDTH 64
+#define HEIGHT 32
+static const int display_width = WIDTH;   // NOLINT
+static const int display_height = HEIGHT; // NOLINT
 
-int add(int a, int b) {
-    return a + b;
+#define NIBBLE(op, n) (((op) >> (12 - 4 * (n))) & 0x0F)
+#define BYTE(op, n) (((op) >> (8 - 8 * (n))) & 0xFF)
+#define ADDR(op) ((op) & 0x0FFF)
+
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+
+uint8_t mem[4096] = {
+    [0x50] = 0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20,          0x60, 0x20, 0x20, 0x70, // 1
+    0xF0,          0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0,          0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90,          0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0,          0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0,          0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0,          0x10, 0x20, 0x40, 0x40, // 7
+    0xF0,          0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0,          0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0,          0x90, 0xF0, 0x90, 0x90, // A
+    0xE0,          0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0,          0x80, 0x80, 0x80, 0xF0, // C
+    0xE0,          0x90, 0x90, 0x90, 0xE0, // D
+    0xF0,          0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0,          0x80, 0xF0, 0x80, 0x80  // F
+};
+uint64_t display[HEIGHT];
+uint16_t pc = 0x200;
+uint16_t idx;
+uint16_t stack_base[16];
+uint16_t *stack_ptr = stack_base;
+uint8_t delay_timer;
+uint8_t sound_timer;
+uint8_t registers[16];
+uint8_t *flag = &registers[15];
+
+void clear_display() {
+  for (int i = 0; i < HEIGHT; i++) {
+    display[i] = 0;
+  }
 }
 
-void print_string() {
-    js_log("Hello, world!");
+void draw_sprite(int x, int y, int height) {
+  *flag = 0;
+  for (int i = 0; i < height; i++) {
+    if (y + i >= HEIGHT) {
+      break;
+    }
+
+    uint64_t mask = mem[idx];
+    if (x <= WIDTH - 8) {
+      mask <<= ((WIDTH - 8) - x);
+    } else {
+      mask >>= (x - (WIDTH - 8));
+    }
+
+    if (display[y + i] & mask) {
+      *flag = 1;
+    }
+    display[y + i] ^= mask;
+  }
+}
+
+void step() {
+  js_log_instruction(pc);
+  uint16_t inst = mem[pc] << 8 | mem[pc + 1];
+  pc += 2;
+
+  js_log_instruction(inst);
+  switch (NIBBLE(inst, 0)) {
+  case 0x0:
+    if (inst != 0x00E0) {
+      js_log_instruction(inst);
+      js_error("Expected instruction 0x00E0");
+    } else {
+      clear_display();
+      js_refresh_display();
+    }
+    break;
+  case 0x1:
+    pc = ADDR(inst);
+    break;
+  case 0x6:
+    registers[NIBBLE(inst, 1)] = BYTE(inst, 1);
+    break;
+  case 0x7:
+    registers[NIBBLE(inst, 1)] += BYTE(inst, 1);
+    break;
+  case 0xA:
+    idx = ADDR(inst);
+    break;
+  case 0xD:
+    uint8_t x = registers[NIBBLE(inst, 1)] % 64;
+    uint8_t y = registers[NIBBLE(inst, 2)] % 32;
+    draw_sprite(x, y, NIBBLE(inst, 3));
+    break;
+  }
 }
--- wasm.js
+++ wasm.js
@@ -2,18 +2,79 @@ const { instance } = await WebAssembly.instantiateStreaming(
     fetch("./chip8.wasm"),
     {
         js: {
-            log: function(ptr) {
-                const memoryBuffer = new Uint8Array(instance.exports.memory.buffer);
-                let end = ptr;
-                while (memoryBuffer[end] !== 0) {
-                    end++;
-                }
-                const bytes = memoryBuffer.subarray(ptr, end);
-                const str = new TextDecoder("latin1").decode(bytes);
-                console.log(str);
-            }
-        },
+            log: log,
+            logInstruction: logInstruction,
+            error: error,
+            refreshDisplay: refreshDisplay,
+        }
     }
 );
-console.log(instance.exports.add(4, 2));
-instance.exports.print_string();
+
+const buffer = instance.exports.memory.buffer;
+const wasmMemory = new Uint8Array(buffer);
+const emulatorMemory = new Uint8Array(buffer, instance.exports.mem, 4096);
+const display = new Uint8Array(buffer, instance.exports.display, instance.exports.display_width * instance.exports.display_height);
+
+function ptrToStr(ptr) {
+    let end = ptr;
+    while (wasmMemory[end] !== 0) {
+        end++;
+    }
+    const bytes = wasmMemory.subarray(ptr, end);
+    return new TextDecoder("latin1").decode(bytes);
+}
+
+function log(ptr) {
+    console.log(ptrToStr(ptr));
+}
+
+function logInstruction(inst) {
+    console.log(`instruction: ${inst.toString(16)}`);
+}
+
+function error(ptr) {
+    running = false;
+    console.error(ptrToStr(ptr));
+}
+
+function refreshDisplay() {
+    console.log("refresh");
+}
+
+const INST_PER_SEC = 700;
+let running = false;
+let last;
+let accumulator = 0;
+
+function start() {
+    running = true;
+    last = document.timeline.currentTime;
+    requestAnimationFrame(step);
+}
+
+function stop() {
+    running = false;
+    accumulator = 0;
+}
+
+function step(now) {
+    if (!running) {
+        return;
+    }
+
+    accumulator += (now - last) / 1000;
+    while (accumulator >= 1 / INST_PER_SEC) {
+        instance.exports.step();
+        accumulator -= 1 / INST_PER_SEC;
+    }
+
+    last = now;
+    requestAnimationFrame(step);
+}
+
+const fileInput = document.getElementById("file-input");
+fileInput.addEventListener("change", async () => {
+    const bytes = await fileInput.files[0].bytes();
+    emulatorMemory.set(bytes, 0x200);
+    start();
+})
